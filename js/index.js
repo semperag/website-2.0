ba9f8c15a1cc5224972ba692345f3559009acfbd
AdiSemper <semperag@dukes.jmu.edu>
2023-03-21 23:24:24 -0400

    comment

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ window.addEventListener('load', function () {
 
     var projects = projectPage.contentDocument.querySelectorAll('.project');
     console.log(projects);
-
+    //
     console.log('here');
     $(projects).each(function( index ) {
         var id = '#' + $(this).attr('id');
